833c8f769d6be176042ad2135911018c19c30771
Jim Crossley <jim@crossleys.org>
2016-08-26 10:38:28 -0400

    HelloWorld example from AMQP.Net Lite
    With the artemis-required 'jms.queue' prefix on the destination name

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Amqp;
 
 namespace ConsoleApplication
 {
@@ -6,7 +8,29 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Run().Wait();
+        }
+        static async Task Run()
+        {
+            string address = "amqp://guest:guest@localhost:5672";
+            string queue = "jms.queue.test";
+
+            Connection connection = await Connection.Factory.CreateAsync(new Address(address));
+            Session session = new Session(connection);
+            SenderLink sender = new SenderLink(session, "test-sender", queue);
+
+            Message message1 = new Message("Hello AMQP!");
+            await sender.SendAsync(message1);
+
+            ReceiverLink receiver = new ReceiverLink(session, "test-receiver", queue);
+            Message message2 = await receiver.ReceiveAsync();
+            Console.WriteLine(message2.GetBody<string>());
+            receiver.Accept(message2);
+
+            await sender.CloseAsync();
+            await receiver.CloseAsync();
+            await session.CloseAsync();
+            await connection.CloseAsync();
         }
     }
 }
